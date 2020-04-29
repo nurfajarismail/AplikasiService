3b6ac4dd59709e18d54aad30c80b74e2b8c29b2b
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-04-29 10:27:31 +0700

    change image again

--- app/src/main/java/com/example/aplikasiservice/WallpaperChangeService.java
+++ app/src/main/java/com/example/aplikasiservice/WallpaperChangeService.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import androidx.annotation.Nullable;
 
 public class WallpaperChangeService extends Service implements Runnable {
-    private int wallpaperID[] = {R.drawable.wal1, R.drawable.wal2};
+    private int wallpaperID[] = {R.drawable.wall1, R.drawable.wall2};
     private int waktu;
     private int FLAG = 0;
     private Thread t;
